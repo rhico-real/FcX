4babfb4e67715a4e41966367510ca0a98f0fcc34
Ehyuu017 <161408413+Ehyuu017@users.noreply.github.com>
2024-10-05 13:41:55 +0800

    Add files via upload

--- Jscript/Navyr.js
+++ Jscript/Navyr.js
@@ -10,3 +10,18 @@
             // Dynamic copyright year
             $('#currentYear').text(new Date().getFullYear());
         });
+
+        // Form validation
+        $('#contactForm').submit(function(event) {
+            event.preventDefault();
+            var name = $('#name').val();
+            var email = $('#email').val();
+            var message = $('#message').val();
+
+            if (name && email && message) {
+                $('#formMessage').text('Thank you for your message! We will get back to you soon.').css('color', 'green');
+                // Here you would typically send the form data to a server
+            } else {
+                $('#formMessage').text('Please fill in all fields.').css('color', 'red');
+            }
+        });
